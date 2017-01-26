b25c9202267474157463121ca7b2992836a5eefa
Jesse Fitzjarrell <jessefitzjarrell@yahoo.com>
2017-01-25 17:47:08 -0800

    Update status to stop when count reaches zero

--- public/bundle.js
+++ public/bundle.js
@@ -25060,6 +25060,11 @@
 	      }
 	    }
 	  },
+
+	  componentWillUnmount: function componentWillUnmount() {
+	    clearInterval(this.timer);
+	    this.timer = undefined;
+	  },
 	  startTimer: function startTimer() {
 	    var _this = this;
 
@@ -25068,6 +25073,9 @@
 	      _this.setState({
 	        count: newCount >= 0 ? newCount : 0
 	      });
+	      if (newCount === 0) {
+	        _this.setState({ countdownStatus: 'stopped' });
+	      }
 	    }, 1000);
 	  },
 	  handleSetCountdown: function handleSetCountdown(seconds) {
@@ -25216,6 +25224,7 @@
 	      _this.props.onStatusChange(newStatus);
 	    };
 	  },
+
 	  render: function render() {
 	    var _this2 = this;
 
